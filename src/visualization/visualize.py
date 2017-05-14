b151c66570bf55d34523a0c2979b76125495d549
Prithvi <prithvinambiar@gmail.com>
2017-05-14 17:48:39 +0530

    Using unix line endings

---
+++ src/visualization/visualize.py
@@ -0,0 +1,18 @@
+# That's an impressive list of imports.
+import numpy as np
+from numpy import linalg
+from numpy.linalg import norm
+from scipy.spatial.distance import squareform, pdist
+
+# We import sklearn.
+import sklearn
+from sklearn.manifold import TSNE
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
+
+digits = load_digits()
+digits.data.shape
+
+print(digits['DESCR'])
+
+print 'hello world'
